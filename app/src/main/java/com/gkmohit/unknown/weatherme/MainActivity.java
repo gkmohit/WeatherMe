ac2d5096e328f689db43728bb227a003ed6e7b7f
gkmohit <gkmohit@gmail.com>
2015-09-18 23:42:18 -0400

    Added refresh button, updated data with realtime information

--- app/src/main/java/com/gkmohit/unknown/weatherme/MainActivity.java
+++ app/src/main/java/com/gkmohit/unknown/weatherme/MainActivity.java
@@ -3,16 +3,23 @@ package com.gkmohit.unknown.weatherme;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
@@ -21,30 +28,114 @@ import com.squareup.okhttp.Response;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends Activity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Main Activity page.
+ * @author gkmohit
+ */
+public class MainActivity extends Activity  {
 
     private CurrentWeather mCurrentWeather;
     public static final String TAG = MainActivity.class.getName();
-    public TextView textView;
+
+    @Bind(R.id.timeLabel) TextView mTimeLabel;
+    @Bind(R.id.temperatureLabel) TextView mTemperatureLabel;
+    @Bind(R.id.humidityValue) TextView mHumidityValue;
+    @Bind(R.id.precpValue) TextView mPrecipValue;
+    @Bind(R.id.summaryLabel) TextView mSummaryLabel;
+    @Bind(R.id.iconImageView) ImageView mIconImageView;
+    @Bind(R.id.locationLabel) TextView mLocationLabel;
+    @Bind(R.id.refreshImageView) ImageView mRefreshImageView;
+    @Bind(R.id.progressBar) ProgressBar mProgressBar;
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mProgressBar.setVisibility(View.INVISIBLE);
 
+
+        checkGPSEnabled();
+        //Latitude and Longitude values.
+        final double latitude = 43.768;
+        final double longitude = -79.345;
+
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getForecast(latitude,longitude);
+            }
+        });
+
+        getForecast(latitude, longitude);
+
+    }
+
+//    protected synchronized void buildGoogleApiClient() {
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API)
+//                .build();
+//    }
+//
+
+    /**
+     * Checks if GPS is enaballed.
+     */
+    public void checkGPSEnabled(){
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+    }
+
+    /**
+     * Creates an alert.
+     */
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS is disabled, please enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog,final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+    /**
+     * Gets a new forecast when called.
+     *
+     * TODO : Pass the queries if needed to change the forecastURL.
+     * @param latitude
+     * @param longitude
+     */
+    private void getForecast(double latitude, double longitude) {
         String apiKey = "58d83f3c25abb4da7c73286532f3264c";
-//        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-//        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//        double longitude = location.getLongitude();
-//        double latitude = location.getLatitude();
-        double latitude = 43.768;
-        double longitude = -79.345;
-        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +  "/" + latitude + "," + longitude;
+        setCityName(latitude, longitude);
+
+
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +  "/" + latitude + "," + longitude + "?units=si";
         Log.v(TAG, "forecastUrl = " + forecastUrl);
         if( isNetworkAvailable()) {
+            toggleRefresh();
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     .url(forecastUrl)
@@ -55,24 +146,42 @@ public class MainActivity extends Activity {
                 @Override
                 public void onFailure(Request request, IOException e) {
                     Log.v(TAG, e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
+                    alertUserAboutError();
                 }
 
                 @Override
                 public void onResponse(Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
+
                     try {
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
                             mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
                         } else {
                             alertUserAboutError();
                         }
 
-                    }
-                    catch (IOException e) {
+                    } catch (IOException e) {
                         Log.e(TAG, "Exception caught: ", e);
-                    }
-                    catch (JSONException e){
+                    } catch (JSONException e) {
                         Log.e(TAG, "Exception caught: ", e);
                     }
                 }
@@ -81,10 +190,59 @@ public class MainActivity extends Activity {
         else{
             Toast.makeText(this, R.string.network_unavailable, Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void setCityName(double latitude, double longitude) {
+        //Getting city name
+        //String returnVal = new String ();
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 10);
+            if (!addressList.isEmpty()) {
+                for (Address address : addressList) {
+                    String result = address.getAdminArea() != null ? address.getAdminArea() : "?";
+                    result += " | ";
+                    result += address.getSubAdminArea() != null ? address.getSubAdminArea() : "?";
+                    result += " | ";
+                    result += address.getLocality() != null ? address.getLocality() : "?";
+                    result += " | ";
+                    result += address.getSubLocality() != null ? address.getSubLocality() : "?";
+                    result += " | ";
+                    result += address.getThoroughfare() != null ? address.getThoroughfare() : "?";
+                    result += " | ";
+                    result += address.getSubThoroughfare() != null ? address.getSubThoroughfare() : "?";
+                    Log.i(TAG, result);
+                    //returnVal = result;
+                    mLocationLabel.setText(result);
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Failed Geocoding.");
+        }
+    }
+
+    private void toggleRefresh() {
+        if(mProgressBar.getVisibility() == View.INVISIBLE){
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRefreshImageView.setVisibility(View.INVISIBLE);
+        } else{
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRefreshImageView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void updateDisplay() {
+        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
+        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it will be ");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+        mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
+        mSummaryLabel.setText(mCurrentWeather.getSummary());
+
+        Drawable drawable  = getResources().getDrawable(mCurrentWeather.getIconId());
+        mIconImageView.setImageDrawable(drawable);
 
     }
 
-    
 
     private boolean isNetworkAvailable() {
 
